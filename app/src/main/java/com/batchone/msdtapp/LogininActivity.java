89d44240ee6479212b800cb785b6499375da2c2f
Waiyanmoehein <waiyanmoehein7@gmail.com>
2019-11-24 13:31:28 +0630

    Ui

---
+++ app/src/main/java/com/batchone/msdtapp/LogininActivity.java
@@ -0,0 +1,14 @@
+package com.batchone.msdtapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LogininActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
