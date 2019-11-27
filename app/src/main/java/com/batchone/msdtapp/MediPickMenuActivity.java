08d4c814a75d9baeb707a5b68d16cc93885ecab4
PhoneMinMyat <phoneminmyat1999@gmail.com>
2019-11-28 03:11:48 +0630

    Add Assignment

---
+++ app/src/main/java/com/batchone/msdtapp/MediPickMenuActivity.java
@@ -0,0 +1,29 @@
+package com.batchone.msdtapp;
+
+import android.os.Bundle;
+
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import androidx.navigation.ui.AppBarConfiguration;
+
+
+public class MediPickMenuActivity extends AppCompatActivity {
+
+
+    @Override
+
+
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medipick_menu);
+
+
+
+
+    }
+
+
+}
