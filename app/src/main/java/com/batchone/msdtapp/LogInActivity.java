5856ea84d24936ba28bcf3ab898bc2123b3be636
PhoneMinMyat <phoneminmyat1999@gmail.com>
2019-11-27 11:33:19 +0630

    MediPickUI

---
+++ app/src/main/java/com/batchone/msdtapp/LogInActivity.java
@@ -0,0 +1,31 @@
+package com.batchone.msdtapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+
+
+public class LogInActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button btnLogin = (Button)findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LogInActivity.this,MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/batchone/msdtapp/MedipickLoginActivity.java
@@ -0,0 +1,14 @@
+package com.batchone.msdtapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MedipickLoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mediapick_login);
+    }
+}
