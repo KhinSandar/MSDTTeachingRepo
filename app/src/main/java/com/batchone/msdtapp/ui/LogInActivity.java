aa1e4606299ce1551e26e777b74169d3af5488c1
hninshweyeemaung-droid <hsym56@gmail.com>
2019-11-29 22:21:20 +0630

    Login UI

---
+++ app/src/main/java/com/batchone/msdtapp/ui/LogInActivity.java
@@ -0,0 +1,17 @@
+package com.batchone.msdtapp.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.batchone.msdtapp.R;
+
+public class LogInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
