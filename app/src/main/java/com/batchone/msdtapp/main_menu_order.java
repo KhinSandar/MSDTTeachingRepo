b0741659f1977d8959f569474a5706cfeb43aebd
Waiyanmoehein <waiyanmoehein7@gmail.com>
2019-12-06 20:06:02 +0630

    Testing

---
+++ app/src/main/java/com/batchone/msdtapp/main_menu_order.java
@@ -0,0 +1,12 @@
+package com.batchone.msdtapp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class main_menu_order extends AppCompatActivity {
+    @Override
+    protected void onCreate(@androidx.annotation.Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
---
+++ app/src/main/java/com/batchone/msdtapp/ui/Reaustant.java
@@ -0,0 +1,16 @@
+package com.batchone.msdtapp.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.batchone.msdtapp.R;
+
+public class Reaustant extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reaustant);
+    }
+}
