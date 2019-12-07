2ea5cd4c7da7f62377f424b70a1df4ba91cdca11
hninshweyeemaung-droid <hsym56@gmail.com>
2019-12-07 11:06:48 +0630

    week5 commit

---
+++ app/src/main/java/com/batchone/msdtapp/ui/ContactUIActivity.java
@@ -0,0 +1,15 @@
+package com.batchone.msdtapp.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.batchone.msdtapp.R;
+
+public class ContactUIActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_ui);
+    }
+}
