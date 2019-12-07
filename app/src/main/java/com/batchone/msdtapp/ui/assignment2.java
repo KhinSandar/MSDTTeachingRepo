e9881b6ebfc0d4b34e22b92f07c314d8b79ec837
aungkomyat <44192004+aungkomyat@users.noreply.github.com>
2019-12-07 12:05:57 +0630

    AKM

---
+++ app/src/main/java/com/batchone/msdtapp/ui/assignment2.java
@@ -0,0 +1,14 @@
+package com.batchone.msdtapp.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class assignment2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assignment2);
+    }
+}
