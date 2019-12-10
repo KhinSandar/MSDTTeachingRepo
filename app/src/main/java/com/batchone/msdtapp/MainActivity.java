478034b258e5c565798af025e0bd777ba321ff65
Hay Mar San <haymarsan11@gmail.com>
2019-12-10 10:19:31 +0630

    UI update: ContactAcitcity Intent, circle image  and CardView Example

--- app/src/main/java/com/batchone/msdtapp/MainActivity.java
+++ app/src/main/java/com/batchone/msdtapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.batchone.msdtapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -38,6 +39,10 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, ContactInfoActivity.class);
+                startActivity(intent);
+
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
