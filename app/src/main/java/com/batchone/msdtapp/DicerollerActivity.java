b12ad9ada6fbffcbdbe33386d25859b1e3a50f70
aungkomyat <44192004+aungkomyat@users.noreply.github.com>
2019-12-14 12:54:41 +0630

    AKM

--- app/src/main/java/com/batchone/msdtapp/DicerollerActivity.java
+++ app/src/main/java/com/batchone/msdtapp/DicerollerActivity.java
@@ -3,7 +3,7 @@ package com.batchone.msdtapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.os.Bundle;
+
 import android.view.View;
 import android.widget.ImageView;
 
